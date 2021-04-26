e065884cef77a53c48f3db3f91c8c922f10eb09c
ait <ait@ex.in>
2021-04-26 21:42:32 +0900

    $emit test / child to parent create event trigger
    [WIP] developer mode

--- src/components/Dev01.vue
+++ src/components/Dev01.vue
@@ -1,12 +1,20 @@
+<!-- 子コンポーネント -->
 <template>
     <div>
         <h3>{{ heroMessage }} / Like: {{ like }} / halfNum: {{ halfNum }}</h3>
+        <p>{{ count }}</p>
+        <button class="btn" @click="inc">Add: {{ button }}</button>
     </div>
 </template>
 <script>
 export default {
     props: {
+        totalNum: Number,
         heroMessage: String,
+        button: {
+            type: String,
+            default: 'Button',
+        },
         // like: Number,
         like: {
             type: Number,
@@ -15,13 +23,21 @@ export default {
         },
     },
     data() {
-        return {}
+        return {
+            count: 0,
+        }
     },
     computed: {
         halfNum() {
             return this.like / 2
         },
     },
-    methods: {},
+    methods: {
+        inc() {
+            // this.$emit('my-Inc', this.heroMessage + ':)')
+            this.$emit('my-Inc')
+            this.count += 1
+        },
+    },
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,21 @@
+<!-- 親コンポーネント -->
+<!-- Example http://jsfiddle.net/StyleEye/tvsL71k2/4/ -->
 <template>
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png" />
-        <dev01 hero-message="I'm developer" :like="likeNumber" />
-        Like: <input type="number" v-model="likeNumber" />
+        <dev01
+            hero-message="I'm developer"
+            :like="likeNumber"
+            @my-Inc="countTotal()"
+        />
+        <dev01
+            hero-message="I'm designer"
+            :like="likeNumber2"
+            @my-Inc="countTotal()"
+        />
+        <dev01 :hero-message="myJob" @my-Inc="countTotal()" button="total" />
+        <!-- Like: <input type="number" v-model="likeNumber" /> -->
+        <h2>Total: {{ total }}</h2>
     </div>
 </template>
 
@@ -14,7 +27,15 @@ import Dev01 from '@/components/Dev01.vue'
     data() {
         return {
             likeNumber: 50,
+            likeNumber2: 70,
+            myJob: 'job...',
+            total: 0,
         }
+    },
+    methods: {
+        countTotal() {
+            this.total = this.total + 1
+        },
     },
     components: {
         Dev01,
