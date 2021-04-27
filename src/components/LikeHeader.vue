a331b2732443f07094467f2e51a58253b863a3ce
ait <ait@ex.in>
2021-04-28 03:04:07 +0900

    Slot Patterns

---
+++ src/components/LikeHeader.vue
@@ -0,0 +1,31 @@
+<template lang="">
+    <div>
+        <slot>
+            def.... child。。。
+        </slot>
+
+        <slot name="title">
+            <div>
+                <h2>LikeHeader {{ headerText }}</h2>
+            </div>
+        </slot>
+
+        <slot name="disc">
+            <p>Hey! (i am child)</p>
+            def if empty
+        </slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LikeHeader',
+    props: ['headerText'],
+}
+</script>
+
+<style lang="scss" scoped>
+h2 {
+    color: orange;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,38 @@
 <template>
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png" />
+        <!-- from LikeHeader  -->
+        <like-header>
+            <!-- <div>
+                こんちくわ
+            </div> -->
+            <!-- Def -->
+            <!-- <template v-slot:default>Def parent !</template> -->
+            <slot></slot>
+
+            <template v-slot:title header-text="monmon">
+                <div>
+                    Title
+                </div>
+            </template>
+            <template v-slot:disc>
+                <div>
+                    Disc parent
+                </div>
+            </template>
+        </like-header>
         <dev01
             hero-message="I'm developer"
             :like="likeNumber"
             @my-Inc="countTotal()"
-        />
+        ></dev01>
         <dev01
-            hero-message="I'm designer"
-            :like="likeNumber2"
+            :hero-message="myJob"
+            :like="likeNumber"
             @my-Inc="countTotal()"
+            button="total"
         />
-        <dev01 :hero-message="myJob" @my-Inc="countTotal()" button="total" />
-        <!-- Like: <input type="number" v-model="likeNumber" /> -->
+        Like: <input type="number" v-model="likeNumber" />
         <h2>Total: {{ total }}</h2>
     </div>
 </template>
@@ -22,6 +42,7 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
 import Dev01 from '@/components/Dev01.vue'
+import LikeHeader from '@/components/LikeHeader.vue'
 
 @Options({
     data() {
@@ -39,6 +60,7 @@ import Dev01 from '@/components/Dev01.vue'
     },
     components: {
         Dev01,
+        LikeHeader,
     },
 })
 export default class Home extends Vue {}
@@ -47,5 +69,8 @@ export default class Home extends Vue {}
 <style lang="scss" scoped>
 img {
     max-width: 48px;
+}
+h2 {
+    color: teal;
 }
 </style>
