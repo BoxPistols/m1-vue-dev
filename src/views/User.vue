93857e1d7bbc37f1ac321d6df7eda2851db31daa
ait <ait@ex.in>
2021-05-08 19:54:21 +0900

    View user List Base on FireBase

--- src/views/User.vue
+++ src/views/User.vue
@@ -8,25 +8,49 @@
             ></div>
             <div class="texts">
                 <h1>{{ user.name }}</h1>
+                <p v-if="myWhispers.length > 1">
+                    {{ myWhispers.length }} whispers
+                </p>
+                <p v-else>{{ myWhispers.length }} whisper</p>
             </div>
+        </div>
+
+        <div class="list">
+            <Item
+                v-for="whisper in orderBy(myWhispers, 'date', -1)"
+                :key="whisper.id"
+                :id="whisper.id"
+                :uid="whisper.uid"
+            />
         </div>
     </div>
 </template>
 
 <script>
 import { db } from '../main'
+import firebase from 'firebase'
+import Item from '@/product/comp/Item'
+import Vue2Filters from 'vue2-filters'
 
 export default {
     data() {
         return {
             user: {},
+            myWhispers: [],
         }
+    },
+    components: {
+        Item,
     },
     firestore() {
         return {
             user: db.collection('users').doc(this.$route.params.uid),
+            myWhispers: db
+                .collection('whispers')
+                .where('uid', '==', this.$route.params.uid),
         }
     },
+    mixins: [Vue2Filters.mixin], // ここを追加
 }
 </script>
 
