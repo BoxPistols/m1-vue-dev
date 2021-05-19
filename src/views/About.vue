b96050a50577ee9f05422278dc3419ee6ceba6e6
ait <ait@ex.in>
2021-05-19 22:04:56 +0900

    [WIP] UI refact

--- src/views/About.vue
+++ src/views/About.vue
@@ -45,7 +45,7 @@ export default {
             border: 2px solid purple;
             padding-bottom: 5vh;
             max-width: 1280px;
-            margin: 2vh auto;
+            margin: .5vh
         }
     }
 }
